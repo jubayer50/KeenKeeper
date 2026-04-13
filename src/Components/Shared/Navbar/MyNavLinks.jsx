b30de46287ad4739ed4c64c99cea6d1972f1474b
jubayer50 <aujubayer1@gmail.com>
2026-04-13 23:25:01 +0600

    navbar buttons icon added and btn function implimented

---
+++ src/Components/Shared/Navbar/MyNavLinks.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router";
+
+const MyNavLinks = ({ to, children }) => {
+  return (
+    <div>
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          `btn font-semibold text-[#64748B] ${isActive ? "bg-[#244D3F] text-white" : ""}`
+        }
+      >
+        {children}
+      </NavLink>
+    </div>
+  );
+};
+
+export default MyNavLinks;
+<NavLink></NavLink>;
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import MyNavLinks from "./MyNavLinks";
+import { Link } from "react-router";
+import { RiHome2Line } from "react-icons/ri";
+import { IoTimeOutline } from "react-icons/io5";
+import { FaChartLine } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div className="py-4 shadow">
       <div className="max-w-360 mx-auto px-2 flex flex-col md:flex-row items-center md:justify-between gap-5">
         <div>
-          <h2 className="font-extrabold text-2xl text-[#1F2937]">
-            Keen<span className="text-[#244D3F]">Keeper</span>
-          </h2>
+          <Link to={"/"}>
+            <h2 className="font-extrabold text-2xl text-[#1F2937]">
+              Keen<span className="text-[#244D3F]">Keeper</span>
+            </h2>
+          </Link>
         </div>
 
-        <div className="flex flex-col gap-3 md:flex-row font-semibold">
-          <button className="btn">Home</button>
-          <button className="btn">Timeline</button>
-          <button className="btn">Stats</button>
+        <div className="flex flex-col gap-3 md:flex-row items-center font-semibold">
+          <MyNavLinks to={"/"}>
+            <RiHome2Line className="text-xl" /> Home
+          </MyNavLinks>
+          <MyNavLinks to={"/timeline"}>
+            <IoTimeOutline className="text-xl font-bold" /> Timeline
+          </MyNavLinks>
+          <MyNavLinks to={"/stats"}>
+            <FaChartLine className="text-xl" /> Stats
+          </MyNavLinks>
         </div>
       </div>
     </div>
